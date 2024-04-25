6cd0e9c42f657e5ed43287506e8f606435fb2de8
Dagaet <alojera15@gmail.com>
2024-04-25 09:59:29 +0100

    fix: repaired connection to database

--- src/db/database.ts
+++ src/db/database.ts
@@ -8,7 +8,7 @@ import { connect as mongooseConnect, connection } from 'mongoose'
 export const connect = async (): Promise<void> => {
   await mongooseConnect(
     // 'mongodb+srv://admin2:adminadmin@prueba.jjuvtzw.mongodb.net/pruebadb?retryWrites=true&w=majority',
-    'mongodb://helpdev:123456@localhost:27017/db_proyect?authSource=admin',
+    'mongodb://admin:admin@localhost:27017/db_proyect?authSource=admin',
   )
 }
 
