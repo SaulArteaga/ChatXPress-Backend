32dd96a1596b20f1c09cac86201d19cacf75f4a3
Dagaet <alojera15@gmail.com>
2024-05-16 14:00:42 +0100

    fix: added middleware authentification in routes

--- src/routes/chats.routes.ts
+++ src/routes/chats.routes.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import ChatsController from '../controller/chats.controller'
+import { auth } from '../middlewares/Auth'
 
 const ChatRoutes = express.Router()
 
-ChatRoutes.get('/chats/active', ChatsController.getChatCount)
-ChatRoutes.get('/chats/:iduser', ChatsController.getChatsFromUser)
-ChatRoutes.post('/chat', ChatsController.retrieveAllMessageFromChat)
-ChatRoutes.put('/chat/:id', ChatsController.updateCurrentChat)
+ChatRoutes.get('/chats/active', auth.ensureAuth, ChatsController.getChatCount)
+ChatRoutes.get('/chats/:iduser', auth.ensureAuth, ChatsController.getChatsFromUser)
+ChatRoutes.post('/chat', auth.ensureAuth, ChatsController.retrieveAllMessageFromChat)
+ChatRoutes.put('/chat/:id', auth.ensureAuth, ChatsController.updateCurrentChat)
 
 export default ChatRoutes
--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
 import MessageController from '../controller/messages.controller'
+import { auth } from '../middlewares/Auth'
 
 const MessageRoutes = express.Router()
 
-MessageRoutes.get('/message/total', MessageController.getTotalMessages)
-MessageRoutes.get('/message/:id', MessageController.getMessageById)
-MessageRoutes.post('/message', MessageController.createMessage)
+MessageRoutes.get('/message/total', auth.ensureAuth, MessageController.getTotalMessages)
+MessageRoutes.get('/message/:id', auth.ensureAuth, MessageController.getMessageById)
+MessageRoutes.post('/message', auth.ensureAuth, MessageController.createMessage)
 
 export default MessageRoutes
