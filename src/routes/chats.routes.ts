6f9743642ee92c313d072e7d2d92942e2bc6c3e3
Dagaet <alojera15@gmail.com>
2024-05-21 12:35:05 +0100

    Docs: documented routes

--- src/routes/chats.routes.ts
+++ src/routes/chats.routes.ts
@@ -4,6 +4,11 @@ import { auth } from '../middlewares/Auth'
 
 const ChatRoutes = express.Router()
 
+/**
+ * All routes are entered with their respective verb methods
+ * and the controller functions to be executed.
+ */
+
 ChatRoutes.get('/chats/active', auth.ensureAuth, ChatsController.getChatCount)
 ChatRoutes.get('/chats/:iduser', auth.ensureAuth, ChatsController.getChatsFromUser)
 ChatRoutes.post('/chat', auth.ensureAuth, ChatsController.retrieveAllMessageFromChat)
--- src/routes/messages.routes.ts
+++ src/routes/messages.routes.ts
@@ -4,6 +4,11 @@ import { auth } from '../middlewares/Auth'
 
 const MessageRoutes = express.Router()
 
+/**
+ * All routes are entered with their respective verb methods
+ * and the controller functions to be executed.
+ */
+
 MessageRoutes.get('/message/total', auth.ensureAuth, MessageController.getTotalMessages)
 MessageRoutes.get('/message/:id', auth.ensureAuth, MessageController.getMessageById)
 MessageRoutes.post('/message', auth.ensureAuth, MessageController.createMessage)
