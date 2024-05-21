6481f53683e14647a561f9150a93403d586c7504
Dagaet <alojera15@gmail.com>
2024-05-21 14:05:54 +0100

    Docs: documented middlewares

--- src/middlewares/Auth.ts
+++ src/middlewares/Auth.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from 'express'
 import { tokenUtils } from '../utils/Token'
 
+/**
+ * This is the middleware for authentication, it checks if the authorization header
+ * that we pass through params is the same as the one on the server.
+ * @param req
+ * @param res
+ * @param next
+ */
 const ensureAuth = (req: Request, res: Response, next: NextFunction) => {
   if (!req.headers.authorization) {
     res.status(401).send({ message: 'La peticion no tiene la cabecera de Authorization' })
